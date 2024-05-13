32c30b8313d2561ffe0ab27da5813c28f1c94715
roni9843 <ronironi98439843@gmail.com>
2024-05-13 17:31:34 +0600

    update

--- config/config.js
+++ config/config.js
@@ -2,7 +2,9 @@ require("dotenv").config();
 
 const config = {
   PORT: process.env.PORT || 8000,
-  DB_CONN: `mongodb+srv://roni9843:${process.env.DB_CONN}@cluster0.utd8f8o.mongodb.net/HasanShop`,
+  DB_CONN: `mongodb+srv://roni9843:roni9843@cluster0.utd8f8o.mongodb.net/HasanShop`,
+  //DB_CONN: `mongodb+srv://roni9843:roni9843@cluster0.utd8f8o.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`,
+  // DB_CONN: `mongodb+srv://roni9843:${process.env.DB_CONN}@cluster0.utd8f8o.mongodb.net/HasanShop`,
 };
 
 module.exports = config;
